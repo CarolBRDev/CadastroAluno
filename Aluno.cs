7c5f4023dceadfd16c4d01aee7353f2d064afe3a
Caroline Ramos <carolbeatrice@gmail.com>
2024-04-01 12:28:41 -0300

    Criação de classe publica com atributos/propriedades publicos/estaticos, contrutor e metodos. Atualização do Program.cs com laço de repetição e condicional

---
+++ Aluno.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroAluno
+{
+    public class Aluno
+    {
+        private static int _indice = 0; //atributo privado da classe -> contará o numero de instancias do objeto
+        private int _id; //atributo privado do objeto - numero identificador da instacia do objeto
+        public string Nome;
+        public string Turma;
+        private static Aluno[] Alunos = new Aluno[10];//inicializa o array Alunos com abjetos da classe Aluno, com capacidade para armazenar até 10 objetos Aluno
+
+        public int Id //propriedade do objeto que permite acesso ao atributo privado do objeto _id e nao permite modificação fora da classe
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+
+        //contrutor da classe 
+        public Aluno(string nome, string turma) 
+        {
+            
+            _id = _indice + 1;
+            Nome = nome;
+            Turma = turma;
+            //_indice++;
+        }
+
+        //metodos
+
+        public static void AdicionarAluno(Aluno aluno)
+        {
+            Alunos[_indice]= aluno; //o array "Alunos", na posição "_indice" recebe "aluno"
+            _indice++;
+            
+        }
+
+        public static Aluno BuscarAluno(int _id) //metodo publico estático que retorna um objeto Aluno a partir do _id (numero identificador do objeto)
+        {
+            _indice = _id - 1; //transforma o numero de id do objeto no valor do´_indice (para localizar o obeto no array - que começa com 0 enquanto o primeiro numero identificador do objeto é 1)       
+            return Alunos[_indice];
+        }
+
+        public static void ExibirAlunos() //metodo publico para exibir a lista completa de alunos cadastrados no array
+        {
+            foreach (var aluno in Alunos) //para cada objeto "aluno" no array "Alunos"
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+
+    public override string ToString()
+        {
+            return $"Id: {_id}\nAluno: {Nome} \nTurma: {Turma}"; //cda vez que pedir para descrever um objeto "aluno", essa sera a forma de representação
+        }
+
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -6,16 +6,9 @@ namespace CadastroAluno
     {
         static void Main(string[] args)
         {
-            //Crie um programa que de ao usuário três opções:
-            //            *Adicionar aluno
-            //           * Buscar aluno pelo índice
-            //           * Exibir todos os alunos(Indice, Id, Nome, Turma)
-            //O programa deve executar em loop até que o usuário peça para sair.
-            //Utilizer atributos estáticos para incrementar o id do usuário.
-            //Utilize uma classe estática com métodos estáticos para armazenar os alunos criados.
-            //      
+            
             int opcao;
-            Console.WriteLine("Escolha a opção que deseja:\n 1) Adicionar Aluno\n 2) Buscar aluno pelo índice\n 3) Exibir todos os alunos cadastrados\n 4) Sair");
+            Console.WriteLine("Escolha a opção que deseja:\n 1) Adicionar Aluno\n 2) Buscar aluno pelo id\n 3) Exibir todos os alunos cadastrados\n 4) Sair");
             opcao = int.Parse(Console.ReadLine());
 
            while (opcao != 4)
@@ -23,13 +16,24 @@ namespace CadastroAluno
                 switch (opcao)
                 {
                     case 1:
-                        Console.WriteLine("Você escolheu Adicionar Aluno.\n"); 
+                        Console.WriteLine("Você escolheu Adicionar Aluno.\n");
+                        Console.WriteLine("Digite o nome do aluno:");
+                        string nome = Console.ReadLine();
+                        Console.WriteLine("Digite a turma do aluno:");
+                        string turma = Console.ReadLine();
+                        Aluno aluno = new Aluno(nome, turma);
+                        Aluno.AdicionarAluno(aluno);
                         break;
                     case 2:
-                        Console.WriteLine("Você escolheu Buscar aluno pelo índice.\n");
+                        Console.WriteLine("Você escolheu Buscar aluno pelo id.\n");
+                        Console.WriteLine("Digite o numero do id:");
+                        int id = int.Parse(Console.ReadLine());
+                        Aluno alunoEncontrado = Aluno.BuscarAluno(id);
+                        Console.WriteLine(alunoEncontrado);
                         break;
                     case 3:
-                        Console.WriteLine("Você escolheu Exibir todos os alunos cadastrados.\n");
+                        Console.WriteLine("Você escolheu Exibir todos os alunos cadastrados.\n"); 
+                        Aluno.ExibirAlunos();
                         break;
                     default:
                         Console.WriteLine("Você deve escolher uma opção. Tente novamente.\n");
